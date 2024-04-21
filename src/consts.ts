c0fb28fa53f009e6f1bad046cf89bfc3950d9979
andrewzn <ondrejzeman23@gmail.com>
2024-04-21 22:02:23 +0200

    fix(meta): update website info

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Personal Tutorial Website';
+export const SITE_DESCRIPTION = 'Website providing quick tutorials on various topics.';
+export const HOMEPAGE_URL = 'https://www.zemn.xyz'
