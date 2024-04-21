f14b90cfe8bc502f43a2200735d582055fd8d440
andrewzn <ondrejzeman23@gmail.com>
2024-04-21 21:36:15 +0200

    chore(dep): removed image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [sitemap(), react(), image()]
+	integrations: [sitemap(), react()]
 });
