52e03371beb3501c5ce16329bc12e86c612f5220
andrewzn <ondrejzeman23@gmail.com>
2024-04-21 22:03:25 +0200

    fix(meta): update website url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+	site: 'https://howto.zemn.xyz',
   integrations: [sitemap(), react(), tailwind()]
 });
