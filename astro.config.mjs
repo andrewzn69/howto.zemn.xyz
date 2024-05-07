6488a3acdc02ce608182f3f5951d4688b18532e5
andrewzn <ondrejzeman23@gmail.com>
2024-05-07 23:43:32 +0200

    feat(astro): changed code theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,28 +3,19 @@ import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://howto.zemn.xyz/',
   integrations: [sitemap(), react(), tailwind()],
   markdown: {
     shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://shiki.style/themes
-      // Alternatively, provide multiple themes
-      // See note below for using dual light/dark themes
-      experimentalThemes: {
-        light: 'dracula',
-        dark: 'nord'
-      },
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://shiki.style/languages
+      theme: 'dracula',
+      // NOTE: Add light mode theme
+      // experimentalThemes: {
+      //   light: 'dracula',
+      //   dark: 'nord'
+      // },
       langs: [],
-      // Enable word wrap to prevent horizontal scrolling
       wrap: true,
-      // Add custom transformers: https://shiki.style/guide/transformers
-      // Find common transformers: https://shiki.style/packages/transformers
       transformers: []
     }
   },
