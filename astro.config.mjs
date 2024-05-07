fc7965164346621eaaaba0b3eff8588935582f22
andrewzn <ondrejzeman23@gmail.com>
2024-05-07 21:38:34 +0200

    chore(utils): utils

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig, sharpImageService } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://howto.zemn.xyz',
-  integrations: [sitemap(), react(), tailwind()]
-});
+  site: 'https://howto.zemn.xyz/',
+  integrations: [sitemap(), react(), tailwind()],
+  image: {
+    service: sharpImageService()
+  },
+
+  markdown: {}
+})
