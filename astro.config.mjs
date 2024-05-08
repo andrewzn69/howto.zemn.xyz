16bd127ad2da165a488d68ad4485feb60b4745d7
andrewzn <ondrejzeman23@gmail.com>
2024-05-08 18:22:18 +0200

    feat(analytics): added vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@ import { defineConfig, sharpImageService } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: { enabled: true }
+	}),
   site: 'https://howto.zemn.xyz/',
   integrations: [sitemap(), react(), tailwind()],
   markdown: {
