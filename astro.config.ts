76c1a1f45371bd2f3b451af6ada7bb70dade1d02
andrewzn <ondrejzeman23@gmail.com>
2024-11-25 19:09:33 +0100

    Huge code block overhaul

--- astro.config.ts
+++ astro.config.ts
@@ -1,33 +1,50 @@
-import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
+import { defineConfig, sharpImageService } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
+import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
 import robotsTxt from 'astro-robots-txt';
-import { defineConfig, sharpImageService } from 'astro/config';
+import expressiveCode, { ExpressiveCodeTheme } from 'astro-expressive-code';
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
 import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
+import fs from 'node:fs';
 
-// https://astro.build/config
+// @expressive-code does not have nord-light theme
+const jsoncString = fs.readFileSync(new URL('./nord-light.json', import.meta.url), 'utf-8');
+const nordlight = ExpressiveCodeTheme.fromJSONString(jsoncString);
+
 export default defineConfig({
 	site: 'https://howto.zemn.xyz/',
 	trailingSlash: 'never',
 	integrations: [
+		expressiveCode({
+			defaultProps: {
+				frame: 'none',
+				showLineNumbers: false,
+			},
+			themeCssSelector: (theme) => `.${theme.type}`,
+			plugins: [pluginLineNumbers()],
+			themes: ['nord', nordlight],
+			styleOverrides: {
+				codeFontFamily: 'jetbrains-mono',
+				uiFontFamily: 'jetbrains-mono',
+				frames: {
+					frameBoxShadowCssValue: '0 4px 20px rgba(0,0,0, 0.1)',
+				},
+				codeFontSize: '0.9rem',
+				codeLineHeight: '1.5rem',
+				codePaddingInline: '1.5rem',
+			},
+		}),
 		sitemap(),
 		react(),
 		mdx(),
-		tailwind({
-			applyBaseStyles: false,
-		}),
+		tailwind(),
 		robotsTxt({ policy: [{ userAgent: '*', disallow: ['/404'] }] }),
 	],
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
-		shikiConfig: {
-			theme: 'one-dark-pro',
-			langs: [],
-			wrap: false,
-			transformers: [],
-		},
 	},
 	image: {
 		service: sharpImageService(),
--- src/components/ui/code/index.ts
+++
@@ -1,5 +0,0 @@
-// @ts-nocheck
-export { default as Block } from './Block.astro';
-export { default as Content } from './Content.astro';
-export { default as Header } from './Header.astro';
-export { default as Root } from './Root.astro';
